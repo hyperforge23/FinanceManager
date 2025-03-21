22aa59d05f275ca1cbe688470bc920ce8feb7e86
itzdharmikpatel <dharmikpatel718@gmail.com>
2025-03-21 19:48:49 +0530

    config file changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/FinanceManager/', // 👈 Important for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
